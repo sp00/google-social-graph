0e4eac895b518627d218ed24bb90a2964f3b8cd4
Mateusz Daleki <mdaleki@gmail.com>
2013-10-09 12:16:44 +0200

    INITIALIZE

---
+++ src/main/java/com/hackaton/google/plus/CircleService.java
@@ -0,0 +1,68 @@
+package com.hackaton.google.plus;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.plus.Plus;
+import com.google.api.services.plus.PlusScopes;
+import com.google.api.services.plus.model.Circle;
+import com.google.api.services.plus.model.CircleFeed;
+import com.roche.google.oauth2.SpringOAuth2Configuration;
+import com.roche.google.util.AuthUtil;
+
+/**
+ * @author mdaleki
+ */
+@Service
+public class CircleService {
+	private static final Logger log = LoggerFactory.getLogger(CircleService.class);
+	public static final String ME = "me";
+	private static List<String> SCOPES =
+			Arrays.asList(PlusScopes.PLUS_ME, PlusScopes.PLUS_STREAM_READ, PlusScopes.PLUS_CIRCLES_WRITE,
+					PlusScopes.PLUS_CIRCLES_READ);
+
+	private final Plus plus;
+	private final SpringOAuth2Configuration oAuth2Configuration;
+
+	@Autowired
+	public CircleService(SpringOAuth2Configuration oAuth2Configuration) {
+		this.oAuth2Configuration = oAuth2Configuration;
+		HttpTransport transport = new NetHttpTransport();
+		JacksonFactory jsonFactory = new JacksonFactory();
+		Credential credential = AuthUtil.createOath2Credential(oAuth2Configuration, SCOPES);
+		this.plus = new Plus.Builder(transport, jsonFactory, credential)
+				.setApplicationName(oAuth2Configuration.getApplicationName()).build();
+	}
+
+	public void loadCircles() throws IOException {
+		loadCircles(ME);
+	}
+
+	public void loadCircles(final String user) throws IOException {
+		CircleFeed circleFeed = buildPlusServiceForUser(user).circles().list(user).execute();
+		for (Circle circle : circleFeed.getItems()) {
+			log.debug("Found circle {} for {}", circle.getDisplayName(), user);
+		}
+
+
+	}
+
+	private Plus buildPlusServiceForUser(String user) {
+		HttpTransport transport = new NetHttpTransport();
+		JacksonFactory jsonFactory = new JacksonFactory();
+		Credential credential = user == null ? AuthUtil.createOath2Credential(oAuth2Configuration, SCOPES) :
+				AuthUtil.createOath2Credential(oAuth2Configuration, user, SCOPES);
+		return new Plus.Builder(transport, jsonFactory, credential)
+				.setApplicationName(oAuth2Configuration.getApplicationName()).build();
+	}
+}
---
+++ src/main/test/java/com/hackaton/google/plus/CircleServiceTest.java
@@ -0,0 +1,37 @@
+package com.hackaton.google.plus;
+
+import java.io.IOException;
+
+import org.fest.assertions.Fail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author mdaleki
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:application-context.xml"})
+public class CircleServiceTest {
+
+	@Autowired
+	private CircleService circleService;
+
+
+	@Test
+	public void shouldLoadCirclesForMe() {
+		//given
+
+		//when
+		try {
+			circleService.loadCircles("zioberm@gene.com");
+		} catch (IOException e) {
+			e.printStackTrace();
+			Fail.fail("Should not happen");
+		}
+
+		//then
+	}
+}
